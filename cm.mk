f48fe0cd8fa8759de0b2a063d9c2a06f3d496b90
Matt Mower <mowerm@gmail.com>
2014-03-10 15:21:13 -0500

    Override product name

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := m7
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/m7/device_m7.mk)
+$(call inherit-product, device/htc/m7/device.mk)
 
 # Device identifier - this must come after all inclusions
 PRODUCT_DEVICE := m7
@@ -13,3 +13,6 @@ PRODUCT_NAME := cm_m7
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One
 PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=m7_google
