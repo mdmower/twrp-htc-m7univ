7c5dcee3f3bf0f9c8b00068e5e7a42ca7628af82
Matt Mower <mowerm@gmail.com>
2014-09-04 02:14:55 -0500

    Add chargeled program for recovery
    A small program to update the LED indicator based on charging status in
    recovery. Build-blocked on RECOVERY_VARIANT=twrp so it doesn't interfere
    with msm8960-common.
    * Green LED for charged
    * Amber LED for charging
    * LED off all other cases
    
    Change-Id: I063013e312f5783f135732f63f8b4e1ba3695915

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/images/battery_fail.png:recovery/root/res/images/charger/battery_fail.png \
     $(LOCAL_PATH)/recovery/images/battery_full.png:recovery/root/res/images/charger/battery_full.png
 
+PRODUCT_PACKAGES += \
+    chargeled
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
