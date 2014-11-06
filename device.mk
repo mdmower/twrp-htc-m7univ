52ae6b97a1fdf58cdc58e850b4d3109a219f101b
Matt Mower <mowerm@gmail.com>
2014-11-06 00:12:07 -0600

    Update makefiles for new build methods
    Move prebuilt files to recovery/root so they are automatically copied by
    recovery_root_private (no need for PRODUCT_COPY_FILES or prebuilt
    modules).
    
    Add chargeled under TARGET_RECOVERY_DEVICE_MODULES so that it becomes a
    dependency of recovery.
    
    Change-Id: I6f6022c6027f22f74dd0810f7701281e69a4b129

--- device.mk
+++ device.mk
@@ -16,20 +16,6 @@ endif
 
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/etc/fstab.qcom:recovery/root/fstab.qcom \
-    $(LOCAL_PATH)/recovery/etc/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
-    $(LOCAL_PATH)/recovery/etc/lpm.rc:recovery/root/lpm.rc \
-    $(LOCAL_PATH)/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
-    $(LOCAL_PATH)/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
-    $(LOCAL_PATH)/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
-    $(LOCAL_PATH)/recovery/sbin/power_test:recovery/root/sbin/power_test
-
-PRODUCT_PACKAGES += \
-    chargeled \
-    offmode_charging_res_images \
-    offmode_charging_warn_res_images
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
